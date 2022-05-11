d3f7f66eb36ad5308f388a246d18b6c48cf84141
Artem Kikhtenko <artemka81@gmail.com>
2022-05-12 01:29:36 +0300

    Added localStorage for contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,14 +4,10 @@ import { Container } from './ui/Container';
 import { ContactForm } from './ContactForm/';
 import { Filter } from './Filter';
 import { ContactList } from './ContactList';
+
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -50,6 +46,19 @@ export class App extends Component {
       ),
     }));
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const jsonContacts = JSON.parse(contacts);
+    if (jsonContacts) {
+      this.setState({ contacts: jsonContacts });
+    }
+  }
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const { contacts, filter } = this.state;
